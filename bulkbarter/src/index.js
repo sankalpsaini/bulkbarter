06d10d203b05f798157d5cdc657c7ed55e583ddf
Sankalp Saini <61366403+sankalpsaini@users.noreply.github.com>
2023-09-16 13:49:22 -0600

    added signup and router

--- bulkbarter/src/index.js
+++ bulkbarter/src/index.js
@@ -1,13 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  // Outlet,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import SignIn from './pages/login';
+import SignUp from './pages/signup';
+import Root from './pages/root';
+
+const router = createBrowserRouter([
+  {
+    element: <Root />,
+    // errorElement: <ErrorPage />,
+    children: [
+      {
+        path: `/`,
+        element: <App />,
+      },
+      {
+        path: `/signup`,
+        element: <SignUp />,
+      },
+      {
+        path: `/signin`,
+        element: <SignIn />,
+      },
+    ]
+  },
+]);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router}  />
   </React.StrictMode>
 );
 
---
+++ bulkbarter/src/pages/root.js
@@ -0,0 +1,21 @@
+// import { Outlet, Link } from "react-router-dom";
+import { Route, Routes } from "react-router";
+// import Navbar from "../components/Navbar";
+
+import App from "../App";
+import SignIn from "./login";
+import SignUp from "./signup";
+
+export default function Root() {
+  return (
+    <>
+      {/* <Navbar /> */}
+      {/* <Outlet /> */}
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="signin" element={<SignIn />} />
+        <Route path="signup" element={<SignUp />} />
+      </Routes>
+    </>
+  );
+}
